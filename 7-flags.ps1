9aca1880458ab95a98e66d031823e6026e9b19b8
pedro27K <27kreached@gmail.com>
2025-09-29 11:01:37 +0200

    añadiendo flags

---
+++ 7-flags.ps1
@@ -0,0 +1,26 @@
+﻿##############################################
+############  Buscar un proceso ##############
+##############################################
+param (
+$proceso
+)
+$f=0
+$p=get-process
+$l=$p.length
+for ($i=0;$i -lt $l;$i++)
+
+{
+
+    if($p[$i].ProcessName -eq $proceso)
+        {
+            $f=1
+        }
+}
+ if($f -eq 1)
+        {
+        Write-Host "Proceso " $proceso " Encontrado "
+        }
+    else
+    {
+    Write-Host "Proceso " $proceso " NO Encontrado "
+    }
---
+++ 7.ps1
@@ -0,0 +1,18 @@
+﻿##############################################
+############  Buscar un proceso ##############
+##############################################
+param (
+$proceso
+)
+$p=get-process
+$l=$p.length
+for ($i=0;$i -lt $l;$i++)
+
+{
+
+if($p[$i].ProcessName -eq $proceso)
+    {
+        Write-Host "proceso encontrado"
+    }
+
+}
