b658bb3f721114648cc39b9d410e8b93cbd56f03
pedro27K <27kreached@gmail.com>
2025-09-29 11:11:47 +0200

    Ultima version Id proceso

---
+++ id-proceso.ps1
@@ -0,0 +1,16 @@
+﻿####ID DE UN PROCESO####
+
+param(
+    [string]$NombreProceso
+)
+
+if (-not $NombreProceso) {
+    $NombreProceso = Read-Host "Introduce el nombre del proceso (ejemplo: notepad)"
+}
+
+try {
+    $proceso = Get-Process -Name $NombreProceso -ErrorAction Stop
+    Write-Output "El proceso '$NombreProceso' tiene la ID: $($proceso.Id)"
+} catch {
+    Write-Output "No se encontró ningún proceso con el nombre '$NombreProceso'"
+}
